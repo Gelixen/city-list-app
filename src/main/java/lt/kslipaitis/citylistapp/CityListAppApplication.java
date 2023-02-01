fb522641f9b22b4df96dbdda3c036b4e76bc0f67
Karolis šlipaitis <slipaitis.karolis@gmail.com>
2023-02-01 12:06:11 +0200

    generate project with initializer

---
+++ src/main/java/lt/kslipaitis/citylistapp/CityListAppApplication.java
@@ -0,0 +1,13 @@
+package lt.kslipaitis.citylistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityListAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityListAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lt/kslipaitis/citylistapp/CityListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.kslipaitis.citylistapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityListAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
